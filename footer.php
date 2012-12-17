b64cdd60a17a009dc4f76eb7497d9c915cf6f1a0
Alex Little <alex@alexlittle.net>
2012-12-17 16:38:55 +0100

    update footer

--- footer.php
+++ footer.php
@@ -3,8 +3,11 @@
 		<div style="clear:both;"></div>
 		<div id="footer">
 			
+			
 			<div id="footer-info">		
-				
+				<div>
+				Digital Campus Ltd is a not-for-profit company registered in England & Wales (Company No. 7629751)
+			</div>
 				<span id="copyright">Copyright <?php echo date('Y'); ?> <?php echo($options['footer_text']); ?> | Powered by<a href="http://www.wordpress.com"> WordPress</a> and <a href="http://www.optimizaresite.org/" title="Optimizare">Optimizare</a></span>
 				<span id="follow-us">Follow us: <a class="rss" href="<?php bloginfo('rss2_url'); ?>">RSS</a> <?php if($options['twitter_url'] <> '') : ?> | <a href="<?php echo($options['twitter_url']); ?>" rel="nofollow"  title="Follow me on twitter.">Twitter</a> <?php endif; ?></span>	
 			</div>
