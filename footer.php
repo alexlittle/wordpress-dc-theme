8a307be5134b8d189509aa7d9f02005a5cae69b8
alexlittle <alex@alexlittle.net>
2013-11-01 16:41:53 +0100

    update footer

--- footer.php
+++ footer.php
@@ -2,18 +2,37 @@
 		<!--footer-->
 		<div style="clear:both;"></div>
 		<div id="footer">
-			
-			
-			<div id="footer-info">		
-				<div>
-				Digital Campus Ltd is a not-for-profit company registered in England & Wales (Company No. 7629751)
+			<div id="footer-content">
+				<div class="footer-col">
+					<h4>Contact Us</h4>
+					<ul>
+						<li><a href="mailto:alex@digital-campus.org">alex@digital-campus.org</a></li>
+					</ul>
+				</div>
+				
+				<div class="footer-col">
+					<h4>About Us</h4>
+					<ul>
+						<li><a href="/team/">Team</a></li>
+						<li><a href="/team/advisory-board/">Advisory Board</a></li>
+						<li><a href="/privacy/">Privacy</a></li>
+					</ul>
+				</div>
+				
+				<div class="footer-col">
+					<h4>Follow Us</h4>
+					<ul>
+						<li><a class="rss" href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/feed-icon-28x28.png"/>RSS</a></li>
+						<li><a href="https://github.com/DigitalCampus/"><img src="<?php bloginfo('template_url'); ?>/images/github-icon.png"/>GitHub</a></li>
+						<?php if($options['twitter_url'] <> '') : ?><li><a href="<?php echo($options['twitter_url']); ?>" rel="nofollow"  title="Follow us on twitter.">Twitter</a></li><?php endif; ?>
+					</ul>
+				</div>
+				<div style="clear:both"></div>
+				<div id="footer-info">		
+					Digital Campus Ltd is a not-for-profit company registered in England & Wales (Company No. 7629751). &copy; <?php echo date('Y'); ?> <?php echo($options['footer_text']); ?>
+				</div>
 			</div>
-				<span id="copyright">Copyright <?php echo date('Y'); ?> <?php echo($options['footer_text']); ?> | Powered by<a href="http://www.wordpress.com"> WordPress</a> and <a href="http://www.optimizaresite.org/" title="Optimizare">Optimizare</a></span>
-				<span id="follow-us">Follow us: <a class="rss" href="<?php bloginfo('rss2_url'); ?>">RSS</a> <?php if($options['twitter_url'] <> '') : ?> | <a href="<?php echo($options['twitter_url']); ?>" rel="nofollow"  title="Follow me on twitter.">Twitter</a> <?php endif; ?></span>	
-			</div>
-		
 		</div>
-		
 </div>
 <!--wrapper end-->
 
