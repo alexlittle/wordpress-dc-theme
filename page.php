384f1f9f3c06b9cb4c7fbefdd9b0d44082cdf1c0
alexlittle <alex@alexlittle.net>
2014-08-04 17:05:48 +0100

    back to bootstrap

--- page.php
+++ page.php
@@ -1,38 +1,55 @@
 <?php get_header(); ?>
-
-		<!--content-->
-		<div id="content">
-		
-			<div id="left-col">
 			
+			<div id="content" class="clearfix row">
+			
+				<div id="main" class="col-sm-8 clearfix" role="main">
 
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-				<div class="post-head">
-
-					<?php if (! is_front_page() ) { ?>	
-						<h1><?php the_title(); ?></h1>
-					<?php } ?>				
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-				</div><!--post-heading end-->
-					
-					<div class="post-entry">
-
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'optimizare' ), 'after' => '' ) ); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
-					</div><!--post-entry end-->
+						<header>
+							
+							<div class="page-header"><h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1></div>
+						
+						</header> <!-- end article header -->
 					
+						<section class="post_content clearfix" itemprop="articleBody">
+							<?php the_content(); ?>
 					
+						</section> <!-- end article section -->
+						
+						<footer>
+			
+							<?php the_tags('<p class="tags"><span class="tags-title">' . __("Tags","wpbootstrap") . ':</span> ', ', ', '</p>'); ?>
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
+					
+					<?php comments_template('',true); ?>
+					
+					<?php endwhile; ?>		
+					
+					<?php else : ?>
+					
+					<article id="post-not-found">
+					    <header>
+					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
+					    </header>
+					    <section class="post_content">
+					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
+					    </section>
+					    <footer>
+					    </footer>
+					</article>
+					
+					<?php endif; ?>
+			
+				</div> <!-- end #main -->
+    
+				<?php get_sidebar(); // sidebar 1 ?>
+    
+			</div> <!-- end #content -->
 
-
-<?php endwhile; ?>
-</div> <!--left-col end-->
-
-<?php get_sidebar(); ?>
-
-</div> <!--content end-->
-	                        <div class="clear"></div>
-</div>
-<!--wrapper end-->
 <?php get_footer(); ?>
